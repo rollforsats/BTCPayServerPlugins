8725106bbd23442da86d2837c5ec8544213fd539
rollforsats <59777267+rollforsats@users.noreply.github.com>
2026-04-02 20:19:42 -0500

    update template plugin to net10.0

--- BTCPayServer.Plugins.Template/Services/MyPluginDbContextFactory.cs
+++ BTCPayServer.Plugins.Template/Services/MyPluginDbContextFactory.cs
@@ -1,8 +1,10 @@
+using System;
 using BTCPayServer.Abstractions.Contracts;
 using BTCPayServer.Abstractions.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Options;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
 
 namespace BTCPayServer.Plugins.Template.Services;
 
@@ -11,11 +13,7 @@ public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<MyPluginDb
     public MyPluginDbContext CreateDbContext(string[] args)
     {
         var builder = new DbContextOptionsBuilder<MyPluginDbContext>();
-
-        // FIXME: Somehow the DateTimeOffset column types get messed up when not using Postgres
-        // https://docs.microsoft.com/en-us/ef/core/managing-schemas/migrations/providers?tabs=dotnet-core-cli
         builder.UseNpgsql("User ID=postgres;Host=127.0.0.1;Port=39372;Database=designtimebtcpay");
-
         return new MyPluginDbContext(builder.Options, true);
     }
 }
@@ -26,10 +24,10 @@ public class MyPluginDbContextFactory : BaseDbContextFactory<MyPluginDbContext>
     {
     }
 
-    public override MyPluginDbContext CreateContext()
+    public override MyPluginDbContext CreateContext(Action<NpgsqlDbContextOptionsBuilder> npgsqlOptionsAction = null)
     {
         var builder = new DbContextOptionsBuilder<MyPluginDbContext>();
-        ConfigureBuilder(builder);
+        ConfigureBuilder(builder, npgsqlOptionsAction);
         return new MyPluginDbContext(builder.Options);
     }
 }
