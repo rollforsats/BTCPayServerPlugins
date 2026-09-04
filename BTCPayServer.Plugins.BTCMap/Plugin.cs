fc01c226f34661f038303e5ee41c6a08647c3d76
rollforsats <59777267+rollforsats@users.noreply.github.com>
2026-09-04 17:30:14 -0500

    chore: update btcpayserver to v2.4.3 and bump plugin to 1.0.2

--- BTCPayServer.Plugins.BTCMap/Plugin.cs
+++ BTCPayServer.Plugins.BTCMap/Plugin.cs
@@ -14,7 +14,7 @@ public class Plugin : BaseBTCPayServerPlugin
 {
     public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
     {
-        new() { Identifier = nameof(BTCPayServer), Condition = ">=2.4.0" }
+        new() { Identifier = nameof(BTCPayServer), Condition = ">=2.4.3" }
     };
 
     public override void Execute(IServiceCollection services)
